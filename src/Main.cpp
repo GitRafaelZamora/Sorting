b98347f39435730c913389ca896a9a928d693daa
Rafael Zamora <rafaelzamora.me@gmail.com>
2017-09-14 17:22:26 +0000

    Added Comments to Algorithm.

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,18 +7,17 @@ using namespace std;
 
 
 int main(void) {
-    Sort sort;
-    int arr[] = {2,13,42,1,10,16,24};
-    int n = 7;
+    Sort sort; // Creating a sort object in order to use different sorting algorithms.
+    int arr[] = {2,13,42,1,10,16,24}; // The array to be sorted.
+    int n = sizeof(arr)/sizeof(arr[0]); // Size of the array.
     
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << endl;
-    }
-    
+    cout << "Unsorted: " << endl;
+    sort.printArray(arr, n); // Printing the unsorted array.
+     
     sort.recursiveInsertionSort(arr, n);
     
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << endl;
-    }
+    cout << "Sorted: " << endl;
+    sort.printArray(arr, n); // Printing the sorted array.
+    
     return 0;
 }
--- src/Sort.cpp
+++ src/Sort.cpp
@@ -7,21 +7,39 @@ using namespace std;
 class Sort {
     public: 
         void recursiveInsertionSort(int* arr, int n);
+        void printArray(int* arr, int n);
 };
     
 void Sort::recursiveInsertionSort(int* arr, int n) {
-    if (n <= 1) {
+    // Base case: 
+    //  Looking for the first index of the array to break out of recursion.
+    if (n <= 1) { 
         return;
     }
     
+    // Calling sort Recursively till n == 1;
     recursiveInsertionSort(arr, n - 1);
     
+    // Storing the current index of the array to 
+    // insert into its correct position.
     int last = arr[n - 1];
+    // Storing the previous index to compare against last.
     int j = n - 2;
     
+    // Checking both j exists and the element to the 
+    // left is bigger than the element we are sorting.
     while (j >= 0 && arr[j] > last) {
-        arr[j+1] = arr[j];
-        j--;
+        arr[j+1] = arr[j]; // Shifting values to the right if theres an issue.
+        j--; // Moving left through array.
     }
-    arr[j + 1] = last;
+    arr[j + 1] = last; // finally inserting element into correct position. 
+}
+
+// Just a simply print array method to make sort easier to easy.
+void Sort::printArray(int* arr, int n) {
+    cout << "[ ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
 }
