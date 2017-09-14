d1137914a081933f013efc92184f79e9aeded8ca
Rafael Zamora <rafaelzamora.me@gmail.com>
2017-09-14 17:08:07 +0000

    Added Recursive Insertion Sort Code.

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,10 +8,17 @@ using namespace std;
 
 int main(void) {
     Sort sort;
-    int arr[] = { 1, 2, 3};
-    int n = 3;
+    int arr[] = {2,13,42,1,10,16,24};
+    int n = 7;
+    
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
     
     sort.recursiveInsertionSort(arr, n);
     
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
     return 0;
 }
--- src/Sort.cpp
+++ src/Sort.cpp
@@ -10,5 +10,18 @@ class Sort {
 };
     
 void Sort::recursiveInsertionSort(int* arr, int n) {
-    cout << "Hello From insertion sort." << endl;
+    if (n <= 1) {
+        return;
+    }
+    
+    recursiveInsertionSort(arr, n - 1);
+    
+    int last = arr[n - 1];
+    int j = n - 2;
+    
+    while (j >= 0 && arr[j] > last) {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = last;
 }
